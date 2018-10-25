d4c73f501502d819af7259b87691dd553c0db4a6
scottjm1 <jasminescott2018@gmail.com>
2018-10-25 16:56:42 -0400

    Working on ColorSensor

--- src/JasmineScott.py
+++ src/JasmineScott.py
@@ -5,14 +5,15 @@
 
 import rosebotics as rb
 import time
-robot = rb.Snatch3rRobot()
 
 def main():
+
     """ Runs YOUR specific part of the project """
     run_test_for_wait_until_intensity_is_less_than()
 
 
 def run_test_for_wait_until_intensity_is_less_than():
+    robot = rb.Snatch3rRobot()
     robot.drive_system.start_moving(50, 50)
     robot.color_sensor.wait_until_intensity_is_less_than(30)
 
