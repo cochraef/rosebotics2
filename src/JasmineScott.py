e00fc923f5f91c7a4f38a180b837667ac3735d7c
scottjm1 <jasminescott2018@gmail.com>
2018-10-28 20:16:07 -0400

    Problem #5

--- src/JasmineScott.py
+++ src/JasmineScott.py
@@ -6,16 +6,24 @@
 import rosebotics as rb
 import time
 
+
 def main():
 
     """ Runs YOUR specific part of the project """
     run_test_for_wait_until_intensity_is_less_than()
+    run_test_for_wait_until_color_is()
 
 
 def run_test_for_wait_until_intensity_is_less_than():
     robot = rb.Snatch3rRobot()
     robot.drive_system.start_moving(50, 50)
     robot.color_sensor.wait_until_intensity_is_less_than(30)
+
+
+def run_test_for_wait_until_color_is():
+    robot = rb.Snatch3rRobot()
+    robot.drive_system.start_moving(20, 20)
+    robot.color_sensor.wait_until_color_is(2)
 
 
 main()
