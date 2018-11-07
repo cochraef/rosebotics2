926f56d041e648970f2d60a12d4d7415265596c3
bowmanj1 <bowmanj1@rose-hulman.edu>
2018-11-07 16:26:25 -0500

    arm functions

--- src/capstone_2_runs_on_robot.py
+++ src/capstone_2_runs_on_robot.py
@@ -7,17 +7,9 @@ Also: responds to Beacon button-presses by beeping, speaking.
 This module runs on the ROBOT.
 It uses MQTT to RECEIVE information from a program running on the LAPTOP.
 
-Authors:  David Mutchler, his colleagues, and PUT_YOUR_NAME_HERE.
+Authors:  David Mutchler, his colleagues, and Jacob Bowman.
 """
-# ------------------------------------------------------------------------------
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.  Then delete this TODO.
-# ------------------------------------------------------------------------------
 
-# ------------------------------------------------------------------------------
-# TODO: 2. With your instructor, review the "big picture" of laptop-robot
-# TODO:    communication, per the comment in mqtt_sender.py.
-# TODO:    Once you understand the "big picture", delete this TODO.
-# ------------------------------------------------------------------------------
 
 import rosebotics_new as rb
 import time
@@ -26,16 +18,16 @@ import ev3dev.ev3 as ev3
 
 
 def main():
-    # --------------------------------------------------------------------------
-    # TODO: 3. Construct a Snatch3rRobot.  Test.  When OK, delete this TODO.
-    # --------------------------------------------------------------------------
 
+    robot = rb.Snatch3rRobot()
     # --------------------------------------------------------------------------
     # TODO: 4. Add code that constructs a   com.MqttClient   that will
     # TODO:    be used to receive commands sent by the laptop.
     # TODO:    Connect it to this robot.  Test.  When OK, delete this TODO.
     # --------------------------------------------------------------------------
-
+    rc = RemoteControlEtc(robot)
+    receiver = com.MqttClient(rc)
+    receiver.connect_to_pc()
     # --------------------------------------------------------------------------
     # TODO: 5. Add a class for your "delegate" object that will handle messages
     # TODO:    sent from the laptop.  Construct an instance of the class and
@@ -58,5 +50,18 @@ def main():
         # ----------------------------------------------------------------------
         time.sleep(0.01)  # For the delegate to do its work
 
+class RemoteControlEtc(object):
+    """
+    Stores the robot.
+        :type robot: rb.Snatch3rRobot
+    """
+    def __init__(self, robot):
+        self.robot = robot
+        pass
+
+    def go_forward(self, speed_str):
+        print('Telling the robot to start moving at', speed_str)
+        speed = int(speed_str)
+        self.robot.drive_system.start_moving(speed, speed)
 
 main()
--- src/rosebotics_new.py
+++ src/rosebotics_new.py
@@ -745,18 +745,16 @@ class ArmAndClaw(object):
         """
         # Done: Do this as STEP 2 of implementing this class.
         self.motor.start_spinning(100)
-
         while True:
             if self.touch_sensor.get_value() == 1:
-                self.motor.stop_spinning()
                 break
-        self.motor.reset_degrees_spun(0)
+        self.motor.stop_spinning()
+        self.motor.reset_degrees_spun()
         self.motor.start_spinning(-100)
         while True:
-            if self.motor.get_degrees_spun() >= 5112:
+            if self.motor.get_degrees_spun() <= -5112:
                 self.motor.stop_spinning()
                 break
-        self.calibrate()
 
     def raise_arm_and_close_claw(self):
         """
@@ -779,6 +777,7 @@ class ArmAndClaw(object):
         Move at a reasonable speed.
         """
         # Done: Do this as STEP 3 of implementing this class.
+        self.motor.reset_degrees_spun()
         self.motor.start_spinning(100)
 
         while True:
